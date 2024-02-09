b0e0b24be98744bc62b4b3be7ccddcb3981183c9
Yury Bursian <dab180996@gmail.com>
2024-02-09 14:19:42 +0200

    hooks added

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,51 @@
 import "./App.css";
 
-function App() {
-  return <div className="App">//...//</div>;
-}
+const App = () => {
+  const [weatherData, setWeatherData] = React.useState({});
+  const [clothingItems, setClothingItems] = React.useState([]);
+  const [activeModal, setActiveModal] = useState();
+  const [createdCard, setCreatedCard] = React.useState(null);
+
+  const hendleCardClick = (card) => {
+    setSelectedCard(card);
+    setActiveModal("preview");
+  };
+
+  const closeAllModals = () => {
+    setActiveModal();
+  };
+
+  React.useEffect(() => {
+    if (location.latitude && location.longtitude) {
+      //api key
+      getForecastWeather(location, secretKey)
+        .then((data) => {
+          setWeatherData(filterDataFromWeatherApi(data));
+        })
+        .catch((err) => console.log(err));
+    }
+  }, []);
+
+  React.useEffect(() => {
+    setClothingItems(defaultClothingItems);
+  }, []);
+
+  return (
+    <div className="page">
+      <div className="page__wrapper">
+        <Header
+          weatherData={weatherData}
+          handleAddClick={() => setActiveModal("create")}
+        />
+        <Main
+          weatherData={weatherData}
+          cards={clothingItems}
+          onCardClick={handleCardClick}
+        />
+        <Footer />
+      </div>
+    </div>
+  );
+};
 
 export default App;
