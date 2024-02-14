484f09f795ea59f6b3b784c9a795837aac556aa1
Yury Bursian <dab180996@gmail.com>
2024-02-14 13:31:30 +0200

    header markup

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,27 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">//...//</div>;
+  return (
+    <div>
+      <header className="header">
+        <div className="header__logo-group">
+          <div>
+            <img src="/images/logo.svg" alt="" />
+          </div>
+          <div>Date</div>
+        </div>
+        <div className="header__avatar-group">
+          <div>
+            <button type="text">+Add new clothes</button>
+          </div>
+          <div>Name</div>
+          <div>
+            <img src="/images/avatar_default.svg" alt="" />
+          </div>
+        </div>
+      </header>
+    </div>
+  );
 }
 
 export default App;
