7b13504697f76c03957f10f97cb11a33557eeb76
Yury Bursian <dab180996@gmail.com>
2024-02-19 22:39:08 +0200

    fonts styling

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,36 +6,12 @@ import ModalWithForm from "../ModalWithForm/ModalWithForm";
 
 function App() {
   return (
-    <div>
+    <div className="page">
       <Header />
       <Main />
       <Footer />
 
-      <ModalWithForm title="New Garment" buttonText="Add Garment">
-        <label className="modal__form-field">
-          Name
-          <input type="text" name="name" minLength="1" maxLength="30" />
-        </label>
-        <label className="modal__form-field">
-          Image URL
-          <input type="url" name="link" minLength="2" />
-        </label>
-        <p>Select the weather type:</p>
-        <ul className="modal__list">
-          <li>
-            <input type="radio" id="hot" value="hot" />
-            <label>Hot</label>
-          </li>
-          <li>
-            <input type="radio" id="warm" value="warm" />
-            <label>Warm</label>
-          </li>
-          <li>
-            <input type="radio" id="cold" value="cold" />
-            <label>Cold</label>
-          </li>
-        </ul>
-      </ModalWithForm>
+      <ModalWithForm title="New Garment" buttonText="Add Garment" />
     </div>
   );
 }
