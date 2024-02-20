43575d84d5f870e457017209b6684a24dafebbb7
Yury Bursian <dab180996@gmail.com>
2024-02-20 13:57:22 +0200

    activeModal hook

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,15 +3,61 @@ import Header from "../Header/Header";
 import Main from "../Main/Main";
 import Footer from "../Footer/Footer";
 import ModalWithForm from "../ModalWithForm/ModalWithForm";
+import { useState } from "react";
 
 function App() {
+  const [activeModal, setActiveModal] = useState("");
   return (
     <div className="page">
       <Header />
       <Main />
       <Footer />
-
-      <ModalWithForm title="New Garment" buttonText="Add Garment" />
+      {activeModal === "create" && (
+        <ModalWithForm title="New Garment" buttonText="Add Garment">
+          <div className="modal__form-field">
+            <label>
+              <h4 className="modal__text">Name</h4>
+              <input
+                type="text"
+                name="name"
+                minLength="1"
+                maxLength="30"
+                required
+                placeholder="Name"
+                className="modal__input"
+              />
+            </label>
+          </div>
+          <div className="modal__form-field">
+            <label>
+              <h4 className="modal__text">Image</h4>
+              <input
+                type="url"
+                name="link"
+                minLength="2"
+                required
+                placeholder="Image URL"
+                className="modal__input"
+              />
+            </label>
+          </div>
+          <p>Select the weather type:</p>
+          <ul className="modal__list">
+            <li>
+              <input type="radio" id="hot" value="hot" />
+              <label>Hot</label>
+            </li>
+            <li>
+              <input type="radio" id="warm" value="warm" />
+              <label>Warm</label>
+            </li>
+            <li>
+              <input type="radio" id="cold" value="cold" />
+              <label>Cold</label>
+            </li>
+          </ul>
+        </ModalWithForm>
+      )}
     </div>
   );
 }
--- src/components/ModalWithForm/ModalWithForm.js
+++ src/components/ModalWithForm/ModalWithForm.js
@@ -11,48 +11,7 @@ const ModalWithForm = ({ children, title, buttonText, onClose, name }) => {
         />
         <form className="modal__form">
           <h3 className="modal__form-title">{title}</h3>
-          <div className="modal__form-field">
-            <label>
-              <h4 className="modal__text">Name</h4>
-              <input
-                type="text"
-                name="name"
-                minLength="1"
-                maxLength="30"
-                required
-                placeholder="Name"
-                className="modal__input"
-              />
-            </label>
-          </div>
-          <div className="modal__form-field">
-            <label>
-              <h4 className="modal__text">Image</h4>
-              <input
-                type="url"
-                name="link"
-                minLength="2"
-                required
-                placeholder="Image URL"
-                className="modal__input"
-              />
-            </label>
-          </div>
-          <p>Select the weather type:</p>
-          <ul className="modal__list">
-            <li>
-              <input type="radio" id="hot" value="hot" />
-              <label>Hot</label>
-            </li>
-            <li>
-              <input type="radio" id="warm" value="warm" />
-              <label>Warm</label>
-            </li>
-            <li>
-              <input type="radio" id="cold" value="cold" />
-              <label>Cold</label>
-            </li>
-          </ul>
+
           {children}
           <button type="submit">{buttonText}</button>
         </form>
