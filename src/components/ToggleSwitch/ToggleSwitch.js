f9aea5ad5134416f656f37d5f3c1ac2b9f5abe7a
Yury Bursian <dab180996@gmail.com>
2024-03-04 20:34:29 +0200

    ToggleSwitch styled

--- src/components/ToggleSwitch/ToggleSwitch.js
+++ src/components/ToggleSwitch/ToggleSwitch.js
@@ -1,13 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ToggleSwitch.css";
 
 const ToggleSwitch = () => {
+  const [currentTempUnit, handleToggleSwitch] = useState("C");
   const handleChange = (e) => {
-    console.log(e);
+    if (currentTempUnit === "C") handleToggleSwitch("F");
+    if (currentTempUnit === "F") handleToggleSwitch("C");
   };
+  console.log(currentTempUnit);
   return (
     <label className="switch">
       <input type="checkbox" className="switch__box" onChange={handleChange} />
+      <span
+        className={
+          currentTempUnit === "F"
+            ? "switch__slider switch__slider_F"
+            : "switch__slider switch__slider_C"
+        }
+      ></span>
+      <p
+        className={`switch__temp_C ${
+          currentTempUnit === "C" && "switch__active"
+        }`}
+      >
+        C
+      </p>
+      <p
+        className={`switch__temp_F ${
+          currentTempUnit === "F" && "switch__active"
+        }`}
+      >
+        F
+      </p>
     </label>
   );
 };
