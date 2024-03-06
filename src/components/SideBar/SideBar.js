0287700b858b9d51c2a83c7b0a83a8055816f1f9
Yury Bursian <dab180996@gmail.com>
2024-03-06 20:06:10 +0200

    Profile styled

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -5,7 +5,7 @@ import avatar from "../../images/avatar_default.svg";
 const SideBar = () => (
   <div className="sidebar">
     <img src={avatar} alt="sidebar avatar" className="sidebar__avatar" />
-    <p className="sidebar__name">Yury</p>
+    <h3 className="sidebar__name">Yury</h3>
   </div>
 );
 
