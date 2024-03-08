da02f9b900ec13af5fb682279bdbea29f70bbd48
Yury Bursian <dab180996@gmail.com>
2024-03-08 20:39:10 +0200

    weatherApi improved

--- src/utils/weatherApi.js
+++ src/utils/weatherApi.js
@@ -1,3 +1,5 @@
+import { handleResponse } from "./api";
+
 const latitude = 32.81;
 const longitude = 34.99;
 const APIkey = "f2829c3c313227659f4adbec810a229f";
@@ -5,13 +7,7 @@ const APIkey = "f2829c3c313227659f4adbec810a229f";
 export const getForecastWeather = () => {
   const weatherApi = fetch(
     `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=imperial&appid=${APIkey}`
-  ).then((res) => {
-    if (res.ok) {
-      return res.json();
-    } else {
-      return Promise.reject(`${res.status}`);
-    }
-  });
+  ).then(handleResponse);
   return weatherApi;
 };
 
