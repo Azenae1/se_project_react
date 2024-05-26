dc564d9c1edcfa2e46bcc8670cddaf9366ab3776
Yury Bursian <dab180996@gmail.com>
2024-05-26 13:57:41 +0300

    baseUrl update

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,7 @@
-export const baseUrl = "http://localhost:3001";
+export const baseUrl =
+  process.env.NODE_ENV === "production"
+    ? "https://api.whattowear.crabdance.com/"
+    : "http://localhost:3001";
 
 export const headers = {
   "Content-Type": "application/json",
